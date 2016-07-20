96b22227b4d1f7d33b63e89f05ca058d3cd54441
Sourav Singh <ssouravsingh12@gmail.com>
2016-07-20 01:09:23 -0700

    Add the code for Blog

---
+++ content/articles/First-post.md
@@ -0,0 +1,6 @@
+Title: My Large Title
+Date: 20-07-2016
+Authors: Sourav Singh
+
+
+This is my First post
---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Sourav Singh'
+SITENAME = u"Sourav's Blog"
+SITEURL = ''
+SITETITLE = AUTHOR
+PATH = 'content'
+THEME = u"../pelican-themes/Flex"
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+
+# Social widget
+SOCIAL = (('Github', 'http://github.com/souravsingh'),
+          ('Twitter', 'http://twitter.com/MrSouravSingh'),
+	('Reddit','http://reddit.com/user/DanSylverstere'))
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
